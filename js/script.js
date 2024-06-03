0e6145a59049ef54ce8128ef8861d6aaa2b071c6
Lost-in-Noise <nancykikvadze@gmail.com>
2024-06-03 15:03:14 +0400

    x shape done

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
 'use strict';
 
 
+//burger bar
+let navBar = document.getElementById('navBar');
+let burgerBar = document.getElementById('burgerBar');
+let navDiv = document.getElementById('navDiv');
+
+burgerBar.addEventListener('click', function() {
+    navBar.classList.toggle('activeNav');
+    burgerBar.classList.toggle('active');
+    navDiv.classList.toggle('mobActive');
+})
