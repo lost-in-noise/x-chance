c656ff429539499b393c161d9abffffec77ccddb
Lost-in-Noise <nancykikvadze@gmail.com>
2024-06-03 16:56:08 +0400

    header is done

--- js/script.js
+++ js/script.js
@@ -1,13 +1,12 @@
-'use strict';
-
+"use strict";
 
 //burger bar
-let navBar = document.getElementById('navBar');
-let burgerBar = document.getElementById('burgerBar');
-let navDiv = document.getElementById('navDiv');
+let navBar = document.getElementById("navBar");
+let burgerBar = document.getElementById("burgerBar");
+let navDiv = document.getElementById("navDiv");
 
-burgerBar.addEventListener('click', function() {
-    navBar.classList.toggle('activeNav');
-    burgerBar.classList.toggle('active');
-    navDiv.classList.toggle('mobActive');
-})
+burgerBar.addEventListener("click", function () {
+  navBar.classList.toggle("activeNav");
+  burgerBar.classList.toggle("active");
+  navDiv.classList.toggle("mobActive");
+});
